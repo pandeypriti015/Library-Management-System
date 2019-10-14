b1e8736b9a3f47cebc69f5b9aad105a90a7d5810
pandeypriti015 <pandeypriti015@gmail.com>
2019-10-14 16:31:43 +0530

    library managment system

--- library/libraryapp/admin.py
+++ library/libraryapp/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book,Student,Author
+from .models import Book,Student,Author,Barrower,Record
+
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('book_name','book_publisher','book_author',)
     search_fields = ('book_name','book_author',)
-    ordering = ('book_name','book_publisher',)
+    ordering = ('book_name',)
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_id','student_name','student_address',
+                    'student_number','book_name','date_of_issue','date_of_return',)
+    ordering = ('student_name',)
+
+
+class BarrowerAdmin(admin.ModelAdmin):
+    list_display = ('id','card','issue_date','due_date','date_return','availability',)
+    ordering = ('due_date',)
+
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('name','book','date_issue','date_return','availability',)
+    ordering = ('book',)
+
+
+
+
+
 admin.site.register(Book,BookAdmin)
+
+
+admin.site.register(Student,StudentAdmin)
+
+
+admin.site.register(Barrower,BarrowerAdmin)
+
+admin.site.register(Record,RecordAdmin)
--- library/libraryapp/models.py
+++ library/libraryapp/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+import datetime
 
 # Create your models here.
 class Book(models.Model):
     book_name = models.CharField(max_length=200)
     book_publisher = models.CharField(max_length=200)
     book_author = models.CharField(max_length=200)
+    book_stock = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.book_name + str(self.book_publisher) + str(self.book_author)
+        return self.book_name
 
 class Student(models.Model):
     student_id = models.AutoField(primary_key=True)
@@ -15,8 +19,8 @@ class Student(models.Model):
     student_address = models.CharField(max_length=200)
     student_number = models.IntegerField()
     book_name = models.ForeignKey(Book,'on_delete=models.CASCADE')
-    date_of_issue = models.DateField()
-    date_of_return = models.DateField()
+    date_of_issue = models.DateField(auto_now_add=True)
+    date_of_return = models.DateField(datetime.datetime.today()+datetime.timedelta(days=7))
 
     def __str__(self):
         return self.student_name
@@ -28,4 +32,36 @@ class Author(models.Model):
 
     def __str__(self):
         return self.author_id
+
+
+class Barrower(models.Model):
+    id = models.AutoField(primary_key=True)
+    card = models.ForeignKey(Student,'on_delete=model.CASCADE')
+    book = models.ForeignKey(Book,'on_delete=model.CASCADE')
+    issue_date = models.DateField(null=True)
+    due_date = models.DateField(null=True)
+    date_return = models.DateField(null=True)
+    availability = models.BooleanField()
+
+    def __str__(self):
+        return self.id
+
+class Record(models.Model):
+    name = models.ForeignKey(Student,'on_delete=model.CASCADE')
+    book = models.ForeignKey(Book,'on_delete=models.CASCADE')
+    date_issue = models.DateField(null=True)
+    date_return = models.DateField(null=True)
+    availability = models.BooleanField()
+
+    def __str__(self):
+        return self.book
+
+
+#class check(models.Model):
+
+@receiver(post_save,sender=Barrower)
+def update_stock(sender,instance,**kwargs):
+    instance.book.BookInstance -= instance.avalibility
+    instance.post.save()
+
 
